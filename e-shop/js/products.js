f665e8e9a7c5cf5c49fb53b89bd92ab0e36b7997
Guilherme Hernandez <guigalhr@gmail.com>
2018-10-10 22:14:32 -0300

    Consulta de produtos com filtro de categoria e pagina (pela url)

--- e-shop/js/products.js
+++ e-shop/js/products.js
@@ -5,28 +5,79 @@ $(function(){
 });
 
 function getProducts(){
+    var urlParams = new URLSearchParams(window.location.search);
+    var categoria = urlParams.get('categoria');
+    var marca = urlParams.get('marca');
+    var pagina = urlParams.get('pagina');
+
+    var data = {
+        page: pagina == null? 0 : pagina,
+        itemsPerPage: 9        
+    }
+
+    if(categoria != null){
+        data.category = categoria;
+    }
+
+    if(marca != null){
+        data.manufacturer = marca; // <-- esse filtro nao funciona (problema da api deles)
+    }
+
     $.ajax({
         type: "GET",
         url: api_produtos + "/products",
-        data: {
-            page: 1,
-            itemsPerPage: 9,            
-        },
+        data: data,
         beforeSend: function (xhr) {
-            xhr.setRequestHeader ("Authorization", "Basic " + btoa("credito:JPfgvQd3"));
+            xhr.setRequestHeader("Authorization", "Basic " + btoa("credito:JPfgvQd3"));
         },
-      }).done(function(data){
-        alert(data);
-      }).fail(function(data)  {
-        alert(data);
+        statusCode: {
+            200: function(data) {
+              for(var i = 0; i < data.content.length; i++){
+                  var item = data.content[i];
+                  addProduct(item);
+              }
+            }
+          }
     }); 
 }
 
-function addProduct(){
+function addProduct(item){
     //cria clone do modelo de produto
     var stub = $("#productStub").clone();
     //tira o id de stub
     stub.removeAttr("id");
+
+    //popula informacoes do produto
+    //nome
+    stub.find(".product-name a").html(item.name);
+    //preco
+    if(item.onSale){
+        //preco em promocao
+        stub.find(".product-price").html("R$" + item.promotionalValue);
+        //preco normal
+        stub.find(".product-price").append(
+            " <del class='product-old-price'>R$" + item.value + "</del>"
+        );
+        //tag de % off
+        stub.find(".product-label span").html("Promoção");
+        var percentOff = (1 - (item.promotionalValue / item.value)) * 100;
+        stub.find(".product-label .sale").html("-" + Math.floor(percentOff) + "%");
+        stub.find(".product-label").toggle();
+        //tags.html("Promoção");
+    }else{
+        //sem promocao
+        stub.find(".product-price").html("R$" + item.value);
+    }
+
+    //imagens
+    if(item.description.startsWith("{")){
+        var json = item.description.replace(new RegExp("'", 'g'), "\"");
+        var extras = JSON.parse(json);
+        if(extras.images.length > 0){
+            stub.find(".product-image").css("background-image", "url(" + extras.images[0] + ")");
+        }
+    }
+
     //adiciona na lista de produtos
     stub.appendTo("#productsList");
     //torna o clone visivel
